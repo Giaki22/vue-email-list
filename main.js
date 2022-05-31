b54564b30a737ecabe894b8e4de90aa93b431551
Giaki22 <giaki22@hotmail.com>
2022-05-31 08:48:47 +0200

    + Milestone 1

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-let app = new Vue ({
-
+let app = new Vue( {
+    el: '#app',
+    data:{
+        mailList: []
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((rep) => {
+                this.mailList.push(rep.data.response)
+            })
+        }
+    }
 })
